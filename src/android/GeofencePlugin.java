9803ab4aa6c45b30cdf5eddba0955b604e9a7151
Lionel Co Ling <lionel.coling@visioncritical.com>
2015-06-18 11:47:04 -0700

    Moved geofence plugin execute() actions to background thread.  Added a (disabled) feature to save logcat output for the app on a schedule.  Also some dead code in there for getting the current location periodically so that the geofence functionality is more accurate.

--- src/android/GeofencePlugin.java
+++ src/android/GeofencePlugin.java
@@ -1,7 +1,16 @@
 package com.cowbell.cordova.geofence;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
@@ -11,8 +20,16 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.app.IntentService;
 import android.content.Intent;
 import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Looper;
 import android.util.Log;
 
 public class GeofencePlugin extends CordovaPlugin {
@@ -21,6 +38,10 @@ public class GeofencePlugin extends CordovaPlugin {
     private Context context;
     protected static Boolean isInBackground = true;
     private static CordovaWebView webView = null;
+    private LocationManager locationManager = null;
+    private LocationListener locationListener = null;
+    private SimpleDateFormat dt = new SimpleDateFormat("yyyyMMddhhmmss");
+    private int pid = android.os.Process.myPid();
 
     /**
      * @param cordova
@@ -35,48 +56,66 @@ public class GeofencePlugin extends CordovaPlugin {
         context = this.cordova.getActivity().getApplicationContext();
         Logger.setLogger(new Logger(TAG, context, false));
         geoNotificationManager = new GeoNotificationManager(context);
+        locationManager = (LocationManager)this.context.getSystemService(Context.LOCATION_SERVICE);
+
+        //configureLocationPinger();
     }
 
     @Override
-    public boolean execute(String action, JSONArray args,
-            CallbackContext callbackContext) throws JSONException {
+    public boolean execute(final String action, final JSONArray args,
+            final CallbackContext callbackContext) throws JSONException {
+
         Log.d(TAG, "GeofencePlugin execute action: " + action + " args: "
                 + args.toString());
 
-        if (action.equals("addOrUpdate")) {
-            List<GeoNotification> geoNotifications = new ArrayList<GeoNotification>();
-            for (int i = 0; i < args.length(); i++) {
-                GeoNotification not = parseFromJSONObject(args.getJSONObject(i));
-                if (not != null) {
-                    geoNotifications.add(not);
+        Runnable run = new Runnable() {
+
+            @Override
+            public void run() {
+                try {
+                    if (action.equals("addOrUpdate")) {
+                        List<GeoNotification> geoNotifications = new ArrayList<GeoNotification>();
+                        for (int i = 0; i < args.length(); i++) {
+                            GeoNotification not = parseFromJSONObject(args.getJSONObject(i));
+                            if (not != null) {
+                                geoNotifications.add(not);
+                            }
+                        }
+                        geoNotificationManager.addGeoNotifications(geoNotifications,
+                                callbackContext);
+                    } else if (action.equals("fireGeofence")) {
+                        String id = args.getString(0);
+                        geoNotificationManager.fireGeofence(id, callbackContext);
+                        // fire geofence
+                    } else if (action.equals("remove")) {
+                        List<String> ids = new ArrayList<String>();
+                        for (int i = 0; i < args.length(); i++) {
+                            ids.add(args.getString(i));
+                        }
+                        geoNotificationManager.removeGeoNotifications(ids, callbackContext);
+                    } else if (action.equals("removeAll")) {
+                        geoNotificationManager.removeAllGeoNotifications(callbackContext);
+                    } else if (action.equals("getWatched")) {
+                        List<GeoNotification> geoNotifications = geoNotificationManager
+                                .getWatched();
+                        callbackContext.success(Gson.get().toJson(geoNotifications));
+                    } else if (action.equals("initialize")) {
+                        callbackContext.success();
+                    } else if (action.equals("deviceready")) {
+                        deviceReady();
+                        callbackContext.success();
+                    } else {
+                        callbackContext.error("Invalid action specified");
+                    }
+                }
+                catch (JSONException jsonEx) {
+                    callbackContext.error("Error parsing JSON parameters provided:" + jsonEx.getMessage());
                 }
             }
-            geoNotificationManager.addGeoNotifications(geoNotifications,
-                    callbackContext);
-        } else if (action.equals("fireGeofence")) {
-            String id = args.getString(0);
-            geoNotificationManager.fireGeofence(id, callbackContext);
-            // fire geofence
-        } else if (action.equals("remove")) {
-            List<String> ids = new ArrayList<String>();
-            for (int i = 0; i < args.length(); i++) {
-                ids.add(args.getString(i));
-            }
-            geoNotificationManager.removeGeoNotifications(ids, callbackContext);
-        } else if (action.equals("removeAll")) {
-            geoNotificationManager.removeAllGeoNotifications(callbackContext);
-        } else if (action.equals("getWatched")) {
-            List<GeoNotification> geoNotifications = geoNotificationManager
-                    .getWatched();
-            callbackContext.success(Gson.get().toJson(geoNotifications));
-        } else if (action.equals("initialize")) {
-            callbackContext.success();
-        } else if (action.equals("deviceready")) {
-			deviceReady();
-            callbackContext.success();
-        } else {
-            return false;
-        }
+        };
+
+        cordova.getThreadPool().execute(run);
+
         return true;
 
     }
@@ -109,6 +148,112 @@ public class GeofencePlugin extends CordovaPlugin {
             Log.d(TAG, "No notifications clicked.");
         } else {
             webView.sendJavascript(js);
+        }
+
+        /*
+        
+        ScheduledExecutorService scheduler =
+                Executors.newSingleThreadScheduledExecutor();
+
+        scheduler.scheduleAtFixedRate
+                (new Runnable() {
+                    public void run() {
+                        saveLogcatLogs();
+                    }
+                }, 1, 2, TimeUnit.MINUTES);
+
+        */
+    }
+
+    private void saveLogcatLogs() {
+        try {
+            String filename = File.separator + "console_" + dt.format(new Date()) + ".log";
+
+            File logFile = new File(context.getExternalFilesDir(null), filename);
+            if (!logFile.exists())
+                logFile.createNewFile();
+
+            Log.d(TAG, "Writing log to path: " + logFile.getAbsolutePath());
+
+            Process process = Runtime.getRuntime().exec("logcat -d -v threadtime | grep " + Integer.toString(pid));
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            String newLine = System.getProperty("line.separator");
+            if (newLine == null) {
+                newLine = "\n";
+            }
+
+            StringBuilder log = new StringBuilder();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                log.append(line);
+                log.append(newLine);
+            }
+
+            FileOutputStream outStream = new FileOutputStream(logFile, true);
+            byte[] buffer = log.toString().getBytes();
+
+            outStream.write(buffer);
+            outStream.close();
+
+            Runtime.getRuntime().exec("logcat -c");
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void configureLocationPinger() {
+        // calling this just for kicks
+        locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        locationListener = new NoOpLocationListener();
+        int interval = 2 * 60 * 1000; // 2 minutes
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, interval, 20, locationListener);
+
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+
+    }
+
+    private class NoOpLocationListenerIntentService extends IntentService {
+
+        private LocationManager locationManager = null;
+
+        public NoOpLocationListenerIntentService() {
+            super("NoOpLocationListenerIntentService");
+
+            locationManager = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
+        }
+
+        @Override
+        protected void onHandleIntent(Intent intent) {
+
+
+            //Log.d(TAG, "Current location is - lat: " + Double.toString(location.getLatitude()) + " long: " + Double.toString(location.getLongitude()));
+        }
+    }
+    private class NoOpLocationListener implements LocationListener {
+
+
+        @Override
+        public void onLocationChanged(Location location) {
+            Log.d(TAG, "Current location is - lat: " + Double.toString(location.getLatitude()) + " long: " + Double.toString(location.getLongitude()));
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
         }
     }
 
